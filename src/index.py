a326a430f0b06244925a0b61c0ccc26b93159182
lehuy0210 <nhokhuypro78999@gmail.com>
2026-08-23 13:51:09 +0700

    fix: sửa dấu kép

--- src/index.py
+++ src/index.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     is_debug = os.environ.get("FLASK_ENV") == "development"
 
