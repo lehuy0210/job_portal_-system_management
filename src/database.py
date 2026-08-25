3d14e23b224ee8fd3648707c672bd22c6cb9c26b
lehuy0210 <nhokhuypro78999@gmail.com>
2026-08-25 09:05:09 +0700

    fix: sửa load env

--- src/database.py
+++ src/database.py
@@ -1,8 +1,10 @@
-# src/database.py
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
